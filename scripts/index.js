b2e5c0a9f2644dd7b7a00e118dcdcf0da97a4320
Garrett Kruis <Gk.kruis@gmail.com>
2024-03-27 19:53:32 -0500

    update js file

--- scripts/index.js
+++ scripts/index.js
@@ -26,16 +26,45 @@ const initialCards = [
   },
 ];
 /* -----------------------------------------------------------------------*/
-/*                               Elements                                 */
+/*                               elements                                 */
 /* -----------------------------------------------------------------------*/
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector("#profile-close-button");
+const profileName = document.querySelector("#profile-title");
+const profileDescr = document.querySelector("#profile-descr");
+const profileTitleInput = document.querySelector("#modal-form-title");
+const profileDescrInput = document.querySelector("#modal-form-descr");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
 
+/* -----------------------------------------------------------------------*/
+/*                               Functions                                */
+/* -----------------------------------------------------------------------*/
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+/* -----------------------------------------------------------------------*/
+/*                               Event Handlers                           */
+/* -----------------------------------------------------------------------*/
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileTitleInput.value;
+  profileDescr.textContent = profileDescrInput.value;
+  closePopup();
+}
+
+/* -----------------------------------------------------------------------*/
+/*                               Event Listeners                          */
+/* -----------------------------------------------------------------------*/
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileName.textContent;
+  profileDescrInput.value = profileDescr.textContent;
   profileEditModal.classList.add("modal__opened");
 });
 
 profileCloseBtn.addEventListener("click", () => {
   profileEditModal.classList.remove("modal__opened");
 });
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
